f2015378c9d40f3a442d4fb609635ef69a304513
Maxime <maxdevvynter@gmail.com>
2024-03-08 21:56:58 +0100

    add about

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,85 @@
 import { AiOutlineMail } from "react-icons/ai";
 import { BsPersonLinesFill } from "react-icons/bs";
 import { FaGithub, FaLinkedinIn } from "react-icons/fa";
+import Image from 'next/image';
 
-export default function Home() {
+export default function Main() {
   return (
-    <div className="w-full h-screen text-center">
-      <div className="max-w-[1440px] w-full h-full mx-auto p-2 flex justify-center items-center">
-        <div>
-          <p className="uppercase text-sm tracking-widest text-gray-600">Let&apos;s build something amazing together.</p>
-          <h1 className="py-4 text-gray-700">
-            Hello, my name is <span className="text-[#5651e5]">Max.</span>
-          </h1>
-          <h1 className="py-2 text-gray-700">I&apos;m Front-End Developer</h1>
-          <p className="py-4 text-gray-500 max-w-[70%] m-auto">
-            I specialize in building fast, responsive, and accessible websites.
-          </p>
-          <div className="flex items-center justify-between max-w-[330px] m-auto py-4">
-            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
-              <FaLinkedinIn />
-            </div>
-            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
-              <FaGithub />
-            </div>
-            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
-              <AiOutlineMail />
-            </div>
-            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
-              <BsPersonLinesFill />
+    <>
+      <div className="w-full h-screen text-center">
+        <div className="max-w-[1440px] w-full h-full mx-auto p-2 flex justify-center items-center">
+          <div>
+            <p className="uppercase text-sm tracking-widest text-gray-600">
+              Let&apos;s build something amazing together.
+            </p>
+            <h1 className="py-4 text-gray-700">
+              Hello, my name is <span className="text-[#5651e5]">Max.</span>
+            </h1>
+            <h1 className="py-2 text-gray-700">I&apos;m Front-End Developer</h1>
+            <p className="py-4 text-gray-500 max-w-[70%] m-auto">
+              I specialize in building fast, responsive, and accessible
+              websites.
+            </p>
+            <div className="flex items-center justify-between max-w-[330px] m-auto py-4">
+              <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+                <FaLinkedinIn />
+              </div>
+              <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+                <FaGithub />
+              </div>
+              <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+                <AiOutlineMail />
+              </div>
+              <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+                <BsPersonLinesFill />
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+      <div className="w-full md:h-screen p-2 flex items-center py-16">
+        <div className="max-w-[1440px] m-auto md:grid grid-cols-3 gap-8">
+          <div className="col-span-2">
+            <p className="uppercase text-xl tracking-widest text-[#5651e5]">About</p>
+            <h2 className="py-4">Who I am</h2>
+            <p className="py-2 text-gray-600">
+              I am a Front-End Developer, specializing in building exceptional
+              websites, applications, and everything in between.
+            </p>
+            <p className="py-2 text-gray-600">
+              My career has seen a transformation from traditional finance and
+              HR roles to the dynamic field of technology. Progressing from
+              junior general accountant to leadership positions such as
+              financial controller and Head of Finance and HR for a fashion
+              company, I&apos;ve honed a robust skill set in financial
+              management, strategic planning, and team leadership. These
+              experiences have honed my ability to analyze complex data and
+              communicate effectively with stakeholders.
+            </p>
+            <p className="py-2 text-gray-600">
+              Motivated by a passion for technology and a drive for continuous
+              growth, I have embraced a new challenge as a Front-End Developer.
+              Through dedicated studies and immersive projects, I&apos;ve become
+              proficient in HTML, CSS, JavaScript, and React, focusing on
+              crafting intuitive and visually appealing user interfaces. This
+              transition in my career reflects my commitment to learning and
+              adapting to new challenges.
+            </p>
+            <p className="py-2 text-gray-600">
+              I am eager to bring my unique blend of skills and experience to a
+              team that values collaboration, creativity, and innovation. I am
+              excited to contribute to projects that push the boundaries of what
+              is possible in web development.
+            </p>
+            <p className="py-2 text-gray-600 underline cursor-pointer">
+              Check out my work and get in touch if you&apos;d like to work
+            </p>
+          </div>
+          <div className="w-full h-auto m-auto shadow-xl shadow-grtay-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease duration-300">
+            <Image className="rounded-xl" src="/assets/character.webp" alt="character" width={400} height={400}/>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
